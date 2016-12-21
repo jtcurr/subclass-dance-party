a148d1164c36a3b8c24a487a4724ac1f7e36728a
jtcurr <johntcurrie@gmail.com>
2016-12-20 20:52:31 -0800

    Added mouseover resize attribute for ed

--- src/lineup.js
+++ src/lineup.js
@@ -1,17 +1,6 @@
 var lineup = function() {
   var windowHeight = 500;
-  //loop thorugh all elements in window.dancers
-  //$(".newDancer").remove();
- // $(".dancer").remove();
- // $(".edDance").remove();
   for (var i = 0; i < window.dancers.length; i++) {
-   // window.dancers[i].top = windowHeight;
-    console.log(window.dancers[i].$node, 'before');
     window.dancers[i].$node.css({ top: windowHeight});
-    console.log(window.dancers[i].$node, 'after');
-
-    //window.dancers[i].styleSettings.top = windowHeight;
-    //$('body').append(window.dancers[i].$node);
   }
-  //reset each top value to the same thing
 };
--- src/makeEd.js
+++ src/makeEd.js
@@ -3,6 +3,7 @@ var makeEd = function(top, left) {
   this.left = left;
   this.$node = $('<img src = "ed.gif" class = "edDance" />');
   this.setPosition(top, left);
+
 };
 makeEd.prototype.setPosition = function (top, left) {
   this.styleSettings = {
